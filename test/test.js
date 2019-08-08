f9e5d5f522e158ed6383a11349c810f82545ce14
tragicDilemma <lspss9950101@gmail.com>
2019-08-08 22:46:02 +0800

    add error handling

--- test/test.js
+++ test/test.js
@@ -7,12 +7,14 @@ function func1(arg, next){
 
 function func2(arg, next){
 	console.log(arg.val++);
-	next();
+	next('error test');
 }
 
-var arg = {val: 0};
+var args = {val: 0};
 
-var tmp = new flow(arg);
-tmp.flow(func1, func2, (arg) => {
-	console.log(arg.val);
-});
+var tmp = new flow();
+tmp.setArgs(args)
+	.setErrorHandler(() => {console.log('Terminated')})
+	.flow(func1, func2, (arg) => {
+		console.log(arg.val);
+	});
